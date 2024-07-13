b52b2ca4ac722f354ea1c3090cf1d41966f77460
MartinezAgullo <pablo.martinez.agullo@gmail.com>
2024-07-13 12:35:15 +0200

    Fix favicons

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,101 +1,44 @@
-function toggleMobileMenu() {
-  $('.navigation-wrapper').toggleClass('visible');
-  $('.btn-mobile-menu__icon').toggleClass('hidden');
-  $('.btn-mobile-close__icon').toggleClass('hidden');
-}
-
 $(document).ready(function () {
-  $('a.panel-button').click(function (e) {
-    const targetId = $(this).attr('href').split('#')[1];
-    
+  $('a.blog-button').click(function (e) {
+    //if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     if ($('.content-wrapper').hasClass('showing')){
-      $('.content-wrapper').removeClass('animated slideInRight slideInLeft');
-      $('.panel-cover').removeClass('panel-cover--collapsed');
-      $('.panel-cover').css('max-width', '100%');
-      $('.panel-cover').animate({'width': '100%'}, 400, 'swing', function () {});
-      $('.content-wrapper').removeClass('showing');
-      history.pushState("", document.title, window.location.pathname + window.location.search);
-      e.preventDefault();
+      $('.content-wrapper').removeClass('animated slideInRight')
+      $('.panel-cover').removeClass('panel-cover--collapsed')
+      $('.panel-cover').css('max-width', '100%')
+      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
+      $('.content-wrapper').removeClass('showing')
+      window.location.hash = '';
+      parent.location.hash = ''
       return;
     }
-    
-    $('.panel-cover').addClass('panel-cover--collapsed');
-    const currentWidth = $('.panel-cover').width();
-    
+    currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
     } else {
-      $('.panel-cover').css('max-width', currentWidth);
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, 'swing', function () {});
+      console.log("hey")
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
-    
-    if (targetId === 'bio') {
-      $('.content-wrapper').addClass('animated slideInLeft');
-      $('#bio-section').show();
-      $('#projects-section').hide();
-    } else if (targetId === 'projects') {
-      $('.content-wrapper').addClass('animated slideInRight');
-      $('#projects-section').show();
-      $('#bio-section').hide();
-    }
-
     $('.content-wrapper').addClass('showing');
-  });
+  })
 
-  if (window.location.hash) {
-    $('a[href="' + window.location.hash + '"]').click();
-  }
+  /*if (window.location.hash && window.location.hash == '#bio') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }*/
 
-  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed');
+  if (window.location.pathname !== '/' && window.location.pathname !== '/index.html') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
   $('.btn-mobile-menu').click(function () {
-    if (!$('.navigation-wrapper').hasClass('animated bounceInDown')){
-        $('.navigation-wrapper').addClass('animated bounceInDown');
-    }
-    toggleMobileMenu();
-  });
 
-  $('.navigation-wrapper .projects-button, .navigation-wrapper .bio-button').click(function () {
-    toggleMobileMenu();
-  });
-});
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
 
-document.addEventListener("DOMContentLoaded", function () {
-  const bioButton = document.querySelector(".bio-button");
-  const codesButton = document.querySelector(".codes-button");
-  const panelCover = document.querySelector(".panel-cover");
-  const bioContent = document.querySelector(".bio-content");
-  const codesContent = document.querySelector(".codes-content");
-
-  if (bioButton) {
-      bioButton.addEventListener("click", function () {
-          if (!panelCover.classList.contains("panel-cover--bio-active")) {
-              panelCover.classList.add("panel-cover--collapsed", "panel-cover--bio-active");
-              bioContent.classList.add("content-wrapper--show");
-              codesContent.classList.remove("content-wrapper--show");
-              window.location.hash = "bio";
-          } else {
-              panelCover.classList.remove("panel-cover--collapsed", "panel-cover--bio-active");
-              bioContent.classList.remove("content-wrapper--show");
-              window.location.hash = "";
-          }
-      });
-  }
-
-  if (codesButton) {
-      codesButton.addEventListener("click", function () {
-          if (!panelCover.classList.contains("panel-cover--codes-active")) {
-              panelCover.classList.add("panel-cover--collapsed", "panel-cover--codes-active");
-              codesContent.classList.add("content-wrapper--show");
-              bioContent.classList.remove("content-wrapper--show");
-              window.location.hash = "codes";
-          } else {
-              panelCover.classList.remove("panel-cover--collapsed", "panel-cover--codes-active");
-              codesContent.classList.remove("content-wrapper--show");
-              window.location.hash = "";
-          }
-      });
-  }
-});
+  $('.navigation-wrapper .projects-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+})
---
+++ bio.md
@@ -0,0 +1,17 @@
+---
+layout: post
+title: Bio
+description: Biography
+permalink: /bio/
+---
+
+Hi! I'm Pablo, PhD in Physics 
+
+
+<iframe width="560" height="315" src="https://youtu.be/9j992iV0GT8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+For more details, see here my [Curriculum Vitae](bio/CV_ENG_.pdf){:target="_blank"}.
+
+<!--
+<img src="/images/Garbi.png" style="max-width:50%"/>
+-->
